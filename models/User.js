ca8d11321b9a96053eae5783e39baafe45491d5a
Lilit Sanjyan <133781980+LSanjyan@users.noreply.github.com>
2024-12-10 15:48:49 -0500

    Commit

--- models/User.js
+++ models/User.js
@@ -1,10 +1,10 @@
-const { Sequelize, sequelize} = require('../db/db');
+const { Sequelize, sequelize } = require("../db/db");
 
-const User = sequelize.define('user', {
-    username: Sequelize.STRING,
-    name: Sequelize.STRING,
-    password: Sequelize.STRING,
-    email: Sequelize.STRING
+const User = sequelize.define("user", {
+  username: Sequelize.STRING,
+  name: Sequelize.STRING,
+  password: Sequelize.STRING,
+  email: Sequelize.STRING,
 });
 
-module.exports = { USER };
+module.exports = { User };
